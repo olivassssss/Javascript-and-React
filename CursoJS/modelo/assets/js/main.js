c86223111ac09fa53b002c8fab412973806ce5db
olivassssss <diogooliveira_96@hotmail.com>
2023-07-03 15:28:19 +0100

    IMC

--- CursoJS/modelo/assets/js/main.js
+++ CursoJS/modelo/assets/js/main.js
@@ -7,52 +7,63 @@ form.addEventListener('submit', function(e) {
     setResultado('Olá Mundo!');
 });
 
-function setResultado(msg) {
+function setResultado() {
     const resultado = document.querySelector('#resultado');
     resultado.innerHTML = '';
     const p = document.createElement('p');
     p.classList.add('paragrafo-resultado');
     p.innerHTML = 'Qualquer coisa';
     resultado.appendChild(p);
+
+    const peso = document.getElementById('peso');
+    const altura = document.getElementById('altura');
+    
+    const conta = [];
+
+    conta.push({
+        kilogramas: peso.value,
+        metros: altura.value,
+    })
+
+    const imc = peso.value / (altura.value * altura.value);
+    const pesoIdeal = 22;
+
+    if (peso.value >= 20 && peso.value <= 500) {
+        if (altura.value >= 1.00 && altura.value <= 2.50) {
+            if (imc < 18.5) {
+                resultado.classList.add('resultado-amarelo');
+                resultado.innerHTML = `Abaixo do peso.<br>O seu peso ideal será de ${pesoIdeal * (altura.value * altura.value)}kg.`;
+            }
+            else if (imc >= 18.5 && imc < 25) {
+                resultado.classList.add('resultado-verde');
+                resultado.innerHTML = `Peso normal.`
+            }
+            else if (imc >= 25 && imc < 30) {
+                resultado.classList.add('resultado-laranja');
+                resultado.innerHTML = `Sobrepeso.<br>O seu peso ideal será de ${pesoIdeal * (altura.value * altura.value)}kg.`
+            }
+            else if (imc >= 30 && imc < 35) {
+                resultado.classList.add('resultado-verm1');
+                resultado.innerHTML = `Obesidade Grau I.<br>O seu peso ideal será de ${pesoIdeal * (altura.value * altura.value)}kg.`
+            }
+            else if (imc >= 35 && imc < 40) {
+                resultado.classList.add('resultado-verm2');
+                resultado.innerHTML = `Obesidade Grau II.<br>O seu peso ideal será de ${pesoIdeal * (altura.value * altura.value)}kg.`
+            }
+            else if (imc >= 40) {
+                resultado.classList.add('resultado-verm3');
+                resultado.innerHTML = `Obesidade Grau III <br>O seu peso ideal será de ${pesoIdeal * (altura.value * altura.value)}kg.`
+            }
+            else {
+                resultado.classList.add('erro');
+                resultado.innerHTML = 'Peso inválido, insira um peso válido'
+            }
+        }
+        else {
+            resultado.innerHTML = 'Altura Inválida, insira uma altura válida';
+        }
+    } 
+    else {
+        resultado.innerHTML = 'Peso inválido, insira um peso válido'
+    }  
 };
-
-// function imc() {
-
-//     const peso = document.getElementById('peso');
-//     const altura = document.getElementById('altura');
-
-    
-//     const conta = [];
-    
-
-//     conta.push({
-//         kilogramas: peso.value,
-//         metros: altura.value,
-//     })
-
-//     const imc = peso.value / (altura.value * altura.value);
-
-//     if (imc < 18.5) {
-//         resultado.innerHTML = 'Abaixo do peso';
-//     }
-//     else if (imc >= 18.5 && imc < 25) {
-//         resultado.innerHTML = 'Peso normal'
-//     }
-//     else if (imc >= 25 && imc < 30) {
-//         resultado.innerHTML = 'Sobrepeso'
-//     }
-//     else if (imc >= 30 && imc < 35) {
-//         resultado.innerHTML = 'Obesidade Grau I'
-//     }
-//     else if (imc >= 35 && imc < 40) {
-//         resultado.innerHTML = 'Obesidade Grau II'
-//     }
-//     else if (imc >= 40) {
-//         resultado.innerHTML = 'Obesidade Grau III'
-//     }
-//     else {
-//         resultado.innerHTML = 'Peso inválido, insira um peso válido'
-//     }
-
-// }
-
