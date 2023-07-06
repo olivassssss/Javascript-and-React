040994954da4ae68de9d44caddc6b00a17d7c430
olivassssss <diogooliveira_96@hotmail.com>
2023-07-06 15:11:50 +0100

    Aula 35

--- CursoJS/aula35/index.js
+++ CursoJS/aula35/index.js
@@ -6,3 +6,17 @@ const elementos = [
     {tag: 'section', texto: 'Frase 4'}, 
 ];
 
+
+const container = document.querySelector('.container');
+const div = document.createElement('div');
+
+
+for (let i = 0; i < elementos.length; i++) {
+    let { tag, texto } = elementos[i];                          // destruturação
+    let tagCriada = document.createElement(tag);                // criação do elemento pela tag indicada no array elementos
+
+    tagCriada.innerHTML = texto;                                // escrever no HTML
+    div.appendChild(tagCriada);                                 // criar um div para inserir os dados das novas tag criadas
+}
+
+container.appendChild(div);                                     // inserir no container já existente o conteudo da Div
