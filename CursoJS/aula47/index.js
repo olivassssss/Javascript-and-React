6f85e43610bb4252101272f7846e4bd094651c1d
olivassssss <diogooliveira_96@hotmail.com>
2023-07-10 14:30:39 +0100

    Timer finished

--- CursoJS/aula47/index.js
+++ CursoJS/aula47/index.js
@@ -1,32 +1,46 @@
 
+function criaHoraDosSegundos(segundos) {
+    const data = new Date(segundos * 1000);
+
+    return data.toLocaleTimeString('pt-PR', {
+        hour12: false,
+        timeZone: 'GMT'
+    });
+}
+
 const relogio = document.querySelector('.relogio');
 const iniciar = document.querySelector('.iniciar');
 const pausar = document.querySelector('.pausar');
 const reiniciar = document.querySelector('.reiniciar');
+let segundos = 0;
+let timer;
 
-iniciar.addEventListener('click', function(e){
-    relogio.innerHTML = iniciarTimer();
-})
-
-pausar.addEventListener('click', function(e){
-    alert('cliquei em pausar')
-})
-
-reiniciar.addEventListener('click', function(e){
-    alert('cliquei em reiniciar')
-})
-
-
-function iniciarTimer() {
-    let data = new Date(0);
-
-    return data.toLocaleTimeString('pt-PT', {
-        hour12: false
-    })
+function iniciarRelogio() {
+    timer = setInterval(function () {
+        segundos++;
+        relogio.innerHTML = criaHoraDosSegundos(segundos);
+    }, 1000)
 }
 
-const timer = setInterval(function () {
-    iniciarTimer();
-}, 1000)
+iniciar.addEventListener('click', function(event){
+    relogio.classList.remove('pausado');
+    clearInterval(timer);
+    iniciarRelogio();
+})
+
+pausar.addEventListener('click', function(event){
+    relogio.classList.add('pausado');
+    clearInterval(timer);
+})
+
+reiniciar.addEventListener('click', function(event){
+    relogio.classList.remove('pausado');
+    clearInterval(timer);
+    relogio.innerHTML = '00:00:00';
+    segundos = 0;
+})
+
+
+
 
 
