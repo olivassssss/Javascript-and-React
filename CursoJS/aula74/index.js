a40f43c992b7a54b7a7d5911620a3d8b1194815b
olivassssss <diogooliveira_96@hotmail.com>
2023-07-15 20:44:25 +0100

    a

--- CursoJS/aula74/index.js
+++ CursoJS/aula74/index.js
@@ -9,6 +9,8 @@ function Pessoa(nome, sobrenome) {
 
 const pessoa1 = new Pessoa('Diogo', 'Oliveira');
 const pessoa2 = new Pessoa('Sara', 'Santos');
+const pessoa3 = new Pessoa('Rosa', 'Oliveira');
 
 console.dir(pessoa1)
 console.dir(pessoa2)
+console.dir(pessoa3.nomeCompleto())
