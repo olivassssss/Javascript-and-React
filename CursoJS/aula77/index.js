08cc55796698446970c59a51ebeea68c2c852491
olivassssss <diogooliveira_96@hotmail.com>
2023-07-19 14:28:16 +0100

    Update index cpf

--- CursoJS/aula77/index.js
+++ CursoJS/aula77/index.js
@@ -20,36 +20,54 @@
     705.484.450-52      - Tem de ser uma string
 */
 
-let cpfLimpo = cpf.replace(/\D+/g, '');
-console.log(cpfLimpo)
-
-// string para array
-
-cpfArray.reduce((ac,val) => ac + Number(val), 0);
-
-(function ValidaCpf(cpfEnviado) {
+function ValidaCPF(cpfEnviado) {
     Object.defineProperty(this, 'cpfLimpo', {
         enumerable: true,
         get: function() {
-            return cpfEnviado.replace(/\D+/g, '')
+            return cpfEnviado.replace(/\D+/g, '');
         }
-    })
-})
+    });
+}
 
-ValidaCpf.prototype.valida() = function() {
+ValidaCPF.prototype.valida = function() {
     if(typeof this.cpfLimpo === 'undefined') return false
     if(this.cpfLimpo.length !== 11) return false
+    if(this.isSequencia()) return false;
 
     const cpfParcial = this.cpfLimpo.slice(0, -2)
     const digito1 = this.criaDigito(cpfParcial)
-    // if(typeof cpfLimpo === ' number')    
+    const digito2 = this.criaDigito(cpfParcial + digito1)
 
-    return true;
+
+    const novoCPF = cpfParcial + digito1 + digito2
+    return novoCPF === this.cpfLimpo;
 }
 
-ValidaCpf.prototype.criaDigito = function(cpfParcial) {
+ValidaCPF.prototype.criaDigito = function(cpfParcial) {
     const cpfArray = Array.from(cpfParcial)
+
+    let regressivo = cpfArray.length + 1;
+    const total = cpfArray.reduce((ac, val) => {
+        ac += (regressivo * Number(val));
+        regressivo--;
+        return ac
+    }, 0)
+
+    // console.log(digito)
+
+    const digito = 11 - (total % 11);
+    return digito > 9 ? '0' : String(digito);
 }
 
-const cpf = new ValidaCpf('705.484.450-52');
-cpf.valida();
+ValidaCPF.prototype.isSequencia = function() {
+    const sequencia = this.cpfLimpo[0].repeat(this.cpfLimpo.length)
+    return sequencia === this.cpfLimpo
+}
+
+const cpf = new ValidaCPF('705.484.450-52');
+if(cpf.valida()) {
+    console.log('Cpf válido')
+} else {
+    console.log('Cpf inválido')
+};
+
