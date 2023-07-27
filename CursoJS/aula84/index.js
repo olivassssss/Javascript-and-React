cb711402f20332b7b8341bb2c89d6586cb308500
olivassssss <diogooliveira_96@hotmail.com>
2023-07-27 15:34:09 +0100

    a

--- CursoJS/aula84/index.js
+++ CursoJS/aula84/index.js
@@ -11,17 +11,47 @@ class ValidaCPF {
     }
 
     éSequencia() {
-        return this.cpfLimpo.charAt(0);
+        return this.cpfLimpo.charAt(0).repeat(11) === this.cpfLimpo;
         // const sequencias = ['11111111111','2']
     }
+
+    geraNovoCPF() {
+        const cpfSemDigitos = this.cpfLimpo.slice(0, -2)
+        const digito1 = this.geraDigito(cpfSemDigitos);
+        const digito2 = this.geraDigito(cpfSemDigitos + digito1);
+        this.novoCPF = cpfSemDigitos + digito1 + digito2
+    }
+
+    static geraDigito(cpfSemDigitos) {
+        let total = 0;
+        let reverso = cpfSemDigitos.lenght + 1;
+
+        for(let stringNumerica of cpfSemDigitos) {
+            total += reverso * Number(stringNumerica);
+            reverso--;
+        }
+        const digito = 11 - (total % 11)
+        return digito <= 9 ? String(digito) : '0'
+    }
+    
 
     valida() {
         if(!this.cpfLimpo) return false;
         if(typeof this.cpfLimpo !== 'string') return false;
         if(this.cpfLimpo.lenght !== 11) return false;
-        return 'cheguei aqui';
+        if(this.éSequencia()) return false;
+        this.geraNovoCPF();
+        
+        return this.novoCPF === this.cpfLimpo;
     }
 }
 
-const validacpf = new ValidaCPF('070.987.720-03')
-console.log(validacpf.valida())
+let validacpf = new ValidaCPF('070.987.720-03')
+// console.log(validacpf.valida())
+
+if (validacpf.valida()) {
+    console.log('CPF VÁLIDO')
+}
+else {
+    console.log('CPF INVÁLIDO')
+}
